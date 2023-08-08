6bcd1ee407030622a7edf459607044ddc44cd6e4
Biny19 <binalfew.tadele@gmail.com>
2023-08-08 07:10:11 -0700

    It's API Advanced project

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-"""Function to count words in all hot posts of a given Reddit subreddit"""
+"""
+Function to count words in all hot posts of a given Reddit subreddit
+"""
 import requests
 
 def count_words(subreddit, word_list, after=None, word_count={}):
-    """Prints counts of given words found in hot posts of a given subreddit
-    Args:
-    subreddit (str): The subreddit to search.
-    word_list (list): The list of words to search for in post titles.
-    instances (obj): Key/value pairs of words/counts.
-    after (str): The parameter for the next page of the API results.
-    count (int): The parameter of results matched thus far.
+    """
+    Prints counts of given words found in hot posts of a given subreddit
     """
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {"User-Agent": "Biny19@alxswe.com"}
